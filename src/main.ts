a8b318751a81873b7aa5d9bee64b3fa2d09567f8
Jonathan <djgrovezone@gmail.com>
2024-02-13 20:55:57 +0100

    Routing migrated

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,43 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { Routes, provideRouter } from '@angular/router';
 
-import { AppModule } from './app/app.module';
+const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('src/app/pages/home/home.component').then((c) => c.HomeComponent),
+  },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('src/app/pages/home/home.component').then((c) => c.HomeComponent),
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('src/app/pages/login/login.component').then(
+        (c) => c.LoginComponent
+      ),
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('src/app/pages/register/register.component').then(
+        (c) => c.RegisterComponent
+      ),
+  },
+  {
+    path: 'notices',
+    loadComponent: () =>
+      import('src/app/pages/notices/notices.component').then(
+        (c) => c.NoticesComponent
+      ),
+  },
+];
 
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [importProvidersFrom(BrowserModule), provideRouter(routes)],
+}).catch((err) => console.error(err));
